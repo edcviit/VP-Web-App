d706647b47f50cb2e9f7afe4e6f596be91b81054
Rohan Vaidya <rohanv.rvaidya@gmail.com>
2024-01-26 19:40:18 +0530

    works?

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
 import "./App.css";
 import RegistrationForm from "./components/Registration";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
 
 function App() {
+  const Navigate = useNavigate();
   return (
     <>
-      <BrowserRouter>
+      
         <Routes>
           {/* <Route path="/*" element={<Landing />} /> */}
           <Route path="/register" element={<RegistrationForm />} />
+          <Route
+            path="/"
+            element={
+              <div
+                onClick={() => {
+                  Navigate("/register");
+                }}
+              >
+                Hello world
+              </div>
+            }
+          />
 
           {/* <p className="text-3xl font-bold underline">VISHWAPRENEUR</p> */}
         </Routes>
-      </BrowserRouter>
     </>
   );
 }
